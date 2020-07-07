e558c326e658f0aead9b33dd3a06183a20cde4f0
Katja Fahrion <katja.fahrion@gmx.de>
2020-07-07 10:52:54 +0200

    vcc731

--- Fit_with_ppxf.py
+++ Fit_with_ppxf.py
@@ -1,26 +1,43 @@
+import matplotlib
 from __future__ import print_function
 
 import pPXF_MUSE.ppxf_MUSE as ppxf_MUSE
 import numpy as np
 import os
-from . import GC_support as sup
+import GC_support as sup
 import glob
 import warnings
 warnings.filterwarnings("ignore")
+matplotlib.use('Agg')
+
+
+def mkdir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+
+def isfile(path):
+    if not os.path.isfile(path):
+        print('File {0} not found!'.format(path))
+        return 0
+    else:
+        return 1
 
 
 if __name__ == "__main__":  # only executed when the program is run by itself and not imported
 
-    galaxy = 'VCC1316'
-    GC_cat_file = '../catalogs/{0}_GC_catalog.fits'
-    out_dir = './ppxf_output/'
+    galaxy = 'VCC731'
+    GC_cat_file = './catalogs/{0}_GC_cat_to_fit.fits'.format(galaxy)
+    out_dir = './ppxf_output/{0}'.format(galaxy)
+    mkdir(out_dir)
+
     n = 100
     GCs = sup.initialize_catalog(GC_cat_file)
     kin_only = True
     alpha_fit = False
 
     for GCi in GCs:
-        print('Fitting GC {0}'.format(int(GCi.ID)))
+        print('Fitting GC {0} of {1}'.format(int(GCi.ID), len(GCs)))
         savebase = '{0}_GC_{1}'.format(galaxy, int(GCi.ID))
         if GCi.SNR >= 8:
             kin_only = False
@@ -30,7 +47,7 @@ if __name__ == "__main__":  # only executed when the program is run by itself an
         else:
             kin_only = True
             age_lim = 12
-        ppxf_MUSE.ppxf_MUSE_MC(GCi.spec, GCi.wave, careful_masking=True, galaxy=galaxy, out_dir=out_dir,
+        ppxf_MUSE.ppxf_MUSE_MC(GCi.spec, GCi.wave, galaxy=galaxy, out_dir=out_dir, vel=1200,
                                filebase=savebase, n=n, kin_only=kin_only, cores=7, lam_range=[4700, 8900],
-                               alpha_fit=alpha_fit, save_plot=True, age_lim=age_lim)
+                               save_plot=True, age_lim=age_lim, mask_file='line_mask.dat')
     # ppxf_MUSE.ppxf_MUSE(spec - spec_bg, wave, plot=True, lam_range = [4700, 8900], galaxy = galaxy, quiet = False)
